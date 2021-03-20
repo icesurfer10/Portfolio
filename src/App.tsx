f1d685672b69fa5d68ac73874210ebb7be5c9710
Thomas West <westthomas@hotmail.co.uk>
2021-03-20 18:47:23 +0000

    Ported basic portfolio over to azure static webapp

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/Layout';
+import Home from './views/Home/Home';
+
+
+function App() {
+  return (
+    <Layout>
+      <Home/>
+    </Layout>
+  );
+}
+
+export default App;
