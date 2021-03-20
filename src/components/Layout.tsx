d56e59ee0ebe0b0ba0fe007524b451f9e79f0f20
Thomas West <westthomas@hotmail.co.uk>
2021-03-20 18:59:50 +0000

    removed unused import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import NavigationMenu from './NavigationMenu';
 
 const Layout: React.FC<React.PropsWithChildren<any>> = ({ children}) => {
   return (
     <div>
-      {/* <NavigationMenu /> */}
-
       <React.Fragment>
         { children }
       </React.Fragment>
